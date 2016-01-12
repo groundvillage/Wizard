9a6fab1e992d8f4ddbedae06b307b7acf3835f20
Tamara <tabernha@htwg-konstanz.de>
2016-01-12 14:06:48 +0100

    small adjustments

--- src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/MatchState/MatchAnalyzingState.java
+++ src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/MatchState/MatchAnalyzingState.java
@@ -2,11 +2,10 @@ package de.htwg.se.wizard.control.gamestate.impl.MainRound.MatchState;
 
 
 import de.htwg.se.wizard.control.GameControl;
-import de.htwg.se.wizard.control.gamestate.IActionState;
+import de.htwg.se.wizard.control.gamestate.impl.ActionSubState;
 import de.htwg.se.wizard.control.gamestate.impl.StateWithSubState;
-import de.htwg.se.wizard.control.gamestate.impl.UserInputState;
 
-public class MatchAnalyzingState extends UserInputState implements IActionState {
+public class MatchAnalyzingState extends ActionSubState {
 
     public MatchAnalyzingState(GameControl controller, StateWithSubState gameState) {
         super(controller, gameState);
@@ -22,4 +21,5 @@ public class MatchAnalyzingState extends UserInputState implements IActionState 
     public String toString() {
         return "MATCH_ANALYZING";
     }
+
 }
