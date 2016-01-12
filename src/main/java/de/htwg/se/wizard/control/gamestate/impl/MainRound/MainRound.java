eb46d956ee749a329f5162a83dfe8b205059638d
Tamara <tabernha@htwg-konstanz.de>
2016-01-12 14:49:22 +0100

    predictionState done so far

--- src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/MainRound.java
+++ src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/MainRound.java
@@ -3,13 +3,14 @@ package de.htwg.se.wizard.control.gamestate.impl.MainRound;
 import de.htwg.se.wizard.control.GameControl;
 import de.htwg.se.wizard.control.gamestate.IUserInputState;
 import de.htwg.se.wizard.control.gamestate.impl.StateWithSubState;
-import de.htwg.se.wizard.model.card.ICard;
 import de.htwg.se.wizard.model.card.carddeck.CardDeck;
 
-import java.util.List;
 import java.util.Map;
 
 public class MainRound extends StateWithSubState implements IUserInputState {
+
+    static final int MAXROUND = 19;
+    static final int PEAKROUND = 10;
 
     private Map<Integer, Integer> predictions;
     private Map<Integer, Integer> points;
@@ -44,16 +45,30 @@ public class MainRound extends StateWithSubState implements IUserInputState {
         return this.controller;
     }
 
-    private int cardsPerPlayer() {
-        return cardsPerPlayer(currentRound, 10);
-    }
-
-    protected int cardsPerPlayer(int curRound, int peakRound) {
-        if (curRound <= peakRound) {
-            return curRound;
+    public int cardsPerPlayer() {
+        if (currentRound <= PEAKROUND) {
+            return currentRound;
         }
         //after 10 rounds, cards are getting less again until 1 card in last round
-        return peakRound - (curRound - peakRound);
+        return PEAKROUND - (currentRound - PEAKROUND);
+    }
+
+    protected int getLastPlayer() {
+        if (this.getFirstPlayer() == 0) {
+            return this.controller.getNumberOfPlayers() - 1;
+        }
+        return firstPlayer - 1;
+    }
+
+    public void setPrediction(int player, int prediction) {
+        this.predictions.put(player, prediction);
+    }
+
+    public void nextRound() {
+        if (this.firstPlayer == this.controller.getNumberOfPlayers() - 1) {
+            this.firstPlayer = 0;
+        }
+        this.firstPlayer += 1;
     }
 
     @Override
--- src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/PredictionState.java
+++ src/main/java/de/htwg/se/wizard/control/gamestate/impl/MainRound/PredictionState.java
@@ -4,32 +4,66 @@ import de.htwg.se.wizard.control.GameControl;
 import de.htwg.se.wizard.control.gamestate.impl.MainRound.MatchState.MatchState;
 import de.htwg.se.wizard.control.gamestate.impl.StateWithSubState;
 import de.htwg.se.wizard.control.gamestate.impl.UserInputSubState;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by Tamara on 11.01.2016.
  */
 public class PredictionState extends UserInputSubState {
 
-
+    private Logger logger = Logger.getLogger("de.htwg.se.wizard.control.gamestate.impl.MainRound.PredictionState");
     private MainRound gameState;
+    private int curPlayer;
+    private int trickSum;
 
     public PredictionState(GameControl controller, StateWithSubState gameState) {
         super(controller, gameState);
         this.gameState = (MainRound) gameState;
+        this.trickSum = 0;
+        this.curPlayer = this.gameState.getFirstPlayer();
+    }
 
-        System.out.println("Prediction state!!");
+    public void setPrediction(int prediction) {
+        if (isValid(prediction)) {
+            gameState.setPrediction(curPlayer, prediction);
+            nextPlayer();
+        } else {
 
-        this.gameState.setSubState(new MatchState(this.gameState));
+        }
+    }
 
-        System.out.println("sollte gleich handeln");
+    private void nextPlayer() {
+        if (this.curPlayer == gameState.getLastPlayer()) {
+            this.gameState.setSubState(new MatchState(this.gameState));
+        }
+    }
 
-        this.controller.notifyObservers();
+    public List<Integer> getValidPredictions() {
+        int maxTricks = gameState.cardsPerPlayer();
+        List<Integer> predictionRange = IntStream.range(0, maxTricks).boxed().collect(Collectors.toList());
+        if (curPlayer != gameState.getLastPlayer()) {
+            return predictionRange;
+        }
+        predictionRange.remove(maxTricks - trickSum);
+        return predictionRange;
     }
 
 
+    private boolean isValid(int prediction) {
+        List<Integer> validPredictions = getValidPredictions();
+        if (validPredictions.contains(prediction)) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public void handleUserInput(String userInput) {
-
+        setPrediction(Integer.parseInt(userInput));
     }
 
     @Override
